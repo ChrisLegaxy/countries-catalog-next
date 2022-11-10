a6bfe9134a8f3d8370bc920f252fdd614b281d29
Chris Legaxy <chrislegaxy@gmail.com>
2022-11-11 02:02:01 +0700

    feat: add completed project replication what was done in Vue 3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+  experimental: {
+    fontLoaders: [
+      { loader: '@next/font/google', options: { subsets: ['latin'] } },
+    ],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org'
+      }
+    ],
+  }
 }
 
 module.exports = nextConfig
